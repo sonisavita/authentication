1a83bb8a246dc9f375c65911f7e0998cb15ef8d8
sonisavita <179831345+sonisavita@users.noreply.github.com>
2025-10-24 22:17:30 +0530

    jwt_jsonWebTokens_impl

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const express = require("express");
-
+const JWT_SECRET = "randomsavitaillgotousa"
+const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-//should return a random long string
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
-    'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', 
-    '5', '6', '7', '8', '9'];
-
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
 
 app.post("/signup", function(req,res){
     const username = req.body.username;
@@ -51,8 +39,11 @@ app.post("/signin", function(req, res){
     }
 
     if (foundUser) {
-        const token = generateToken();
-        foundUser.token = token;
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET); // convert their username over to a jwt
+
+        //foundUser.token = token;
         res.json({
             token: token
         })
@@ -64,6 +55,31 @@ app.post("/signin", function(req, res){
     console.log(users)
     
 });
+
+app.get("/me", function(req,res){
+    const token = req.headers.token //jwt
+    const decodedInformation = jwt.verify(token, JWT_SECRET); // {username: "savita@gmall.com"}
+    const username = decodedInformation.username
+
+    let foundUser = null;
+
+    for(let i = 0; i < users.length; i++){
+        if(users[i].token == token) {
+            foundUser = users[i]
+        }
+    }
+
+    if (foundUser) {
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    } else {
+        res.json({
+            message: "token invalid"
+        })
+    }
+})
 
 app.listen(3000); // http server is listening on port 3000
 
